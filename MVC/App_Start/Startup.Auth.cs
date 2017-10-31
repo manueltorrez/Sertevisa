0fedac2212f9ae586279c6958f6f331cb8f51bd6
Valiente8 <valiente.andres@yahoo.com>
2017-10-30 18:19:50 -0600

    Preparacion para controladores

--- MVC/App_Start/Startup.Auth.cs
+++ MVC/App_Start/Startup.Auth.cs
@@ -15,7 +15,7 @@ namespace MVC
         public void ConfigureAuth(IAppBuilder app)
         {
             // Configure el contexto de base de datos, el administrador de usuarios y el administrador de inicios de sesión para usar una única instancia por solicitud
-            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext(SecurityContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
             app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
 
--- MVC/Contexto/ModeloContexto.cs
+++ MVC/Contexto/ModeloContexto.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BibliotecaClases.Contexto
+namespace MVC.Contexto
 {
     public class ModeloContexto : DbContext
     {
