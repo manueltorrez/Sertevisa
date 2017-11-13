524d95bedc9dfb630eebe81285b2149bb168bb78
Oswaldo Chamorro <oswaldochf1@hotmail.com>
2017-11-13 15:56:40 -0600

    Correcciones

--- MVC/Migrations/Configuration.cs
+++ MVC/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace MVC.Migrations
 {
+    using MVC.Entidades;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -27,8 +28,27 @@ namespace MVC.Migrations
             //    );
             //
 
+            context.Marcas.AddOrUpdate(
+                  p => p.Nombre,
+                  new Marca { Nombre = "Sony" },
+                  new Marca { Nombre = "Samsung" },
+                  new Marca { Nombre = "LG" },
+                  new Marca { Nombre = "Dell" },
+                  new Marca { Nombre = "Toshiba" },
+                  new Marca { Nombre = "LG" },
+                  new Marca { Nombre = "Huawei" }
+                );
+
+            context.Estados.AddOrUpdate(
+                  p => p.Descripcion,
+                  new Estado { Descripcion = "Devolución" },
+                  new Estado { Descripcion = "Sin revisar" },
+                  new Estado { Descripcion = "Propiedad de Sertevisa" },
+                  new Estado { Descripcion = "Reparado - No entregado" },
+                  new Estado { Descripcion = "Reparado - Entregado" }
 
 
+                );
 
 
         }
